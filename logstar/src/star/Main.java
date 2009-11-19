fd9200a50bb85fcfa8e2b03e7932d16c9f41b0c3
dmitry.s.mamonov <dmitry.s.mamonov@f44d2e4a-d4d6-11de-9196-2db6e9c7c7ae>
2009-11-19 07:09:29 +0000

    Initial commit

---
+++ logstar/src/star/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package star;
+
+import analysis.AggregateTraces;
+import java.io.IOException;
+
+/**
+ *
+ * @author root
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        AggregateTraces.main(args);
+    }
+
+}
